a003db9fcbe70fa976dfbe95003f136c852d90c9
nageetaw <nigeetawadhwani@gmail.com>
2022-08-05 16:54:01 +0500

    build errors resolved

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,41 +1,28 @@
-import "./Introduction.css";
-import { useHistory, usehistory } from "react-router-dom";
-export default function Introduction() {
-  const history = useHistory();
-  function handleClick() {
-    history.push("./Register");
-  }
-  return (
-    <div className="container-fluid introPlot ">
-      <div className="row mt-5">
-        <div className="col col-md-5 offset-md-1">
-          <p className="mainMessage">Be a part of TechFest today! </p>
+import './Introduction.css'
+import {useHistory, usehistory} from 'react-router-dom'
+export default function Introduction(){
 
-          <p className="mainMessageDetail">
-            Register yourself today and get full time support and career advice
-            from our experts
-          </p>
-          <button
-            className="d-inline btn loginBtn text-light rounded-pill px-4 mx-1"
-            data-toggle="tooltip"
-            data-placement="bottom"
-            title="want to learn ?"
-            onClick={handleClick}
-          >
-            Join as a Learner
-          </button>
-          <button
-            className="d-inline btn  registerBtn rounded-pill px-4 mx-1"
-            data-toggle="tooltip"
-            data-placement="bottom"
-            title="want to contribute ?"
-            onClick={handleClick}
-          >
-            Become a contributor
-          </button>
+    const history=useHistory()
+    function handleClick(){
+        history.push('./Register')
+    }
+    return(
+        <div className="container-fluid introPlot ">
+
+        <div className="row mt-5">
+            <div className="col col-md-5 offset-md-1">
+                <p className="mainMessage">Join Siba Overflow Today </p>
+
+                <p className="mainMessageDetail">
+                    Register yourself today and get full time support and career advice from our experts
+                      </p>
+                      <button className="d-inline btn loginBtn text-light rounded-pill px-4 mx-1" data-toggle="tooltip" data-placement="bottom" title="want to learn ?" onClick={handleClick}>Join as a Learner</button>
+                      <button className="d-inline btn  registerBtn rounded-pill px-4 mx-1" data-toggle="tooltip" data-placement="bottom" title="want to contribute ?" onClick={handleClick}>Become a contributor</button>     
+            </div>
+            <div className="col col-md-5 offset-md-1">
+
+            </div>
         </div>
-        <div className="col col-md-5 offset-md-1"></div>
-      </div>
-    </div>
-  );
+     </div>
+    )
 }
